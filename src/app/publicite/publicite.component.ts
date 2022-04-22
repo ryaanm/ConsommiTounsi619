a54d44d9018a72b0f7df0531adeb1953775216f1
unknown <merghed.rayen@esprit.tn>
2022-04-22 15:53:06 +0100

    view fix

--- src/app/publicite/publicite.component.ts
+++ src/app/publicite/publicite.component.ts
@@ -49,4 +49,26 @@ export class PubliciteComponent implements OnInit {
      })
 
 }
+open(content: any) {
+  this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.closeResult = `Closed with: ${result}`;
+  }, (reason) => {
+    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+  closeForm(){
+
+  }
+  cancel(){
+    this.form = false;
+  }
 }
